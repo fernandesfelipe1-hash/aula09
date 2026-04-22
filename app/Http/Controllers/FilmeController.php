e2f78b9cfa8dc619e346af82c2dbd798343239f8
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2026-04-22 19:40:55 -0300

    adds initial code

---
+++ app/Http/Controllers/FilmeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Filme;
+
+class FilmeController extends Controller
+{
+    public function index()
+    {
+        $filmes = Filme::orderBy('titulo')->paginate(10);
+
+        return view('filmes.index', compact('filmes'));
+    }
+
+    public function create()
+    {
+        return view('filmes.create');
+    }
+
+    public function store(Request $request)
+    {
+        Filme::create($request->all());
+
+        return redirect()
+            ->route('filmes.index')
+            ->with('success', 'Filme cadastrado com sucesso.');
+    }
+
+    public function show(Filme $filme)
+    {
+        return view('filmes.show', compact('filme'));
+    }
+
+    public function edit(Filme $filme)
+    {
+        return view('filmes.edit', compact('filme'));
+    }
+
+    public function update(Request $request, Filme $filme)
+    {
+        $filme->update($request->all());
+
+        return redirect()
+            ->route('filmes.index')
+            ->with('success', 'Filme atualizado com sucesso.');
+    }
+
+    public function destroy(Filme $filme)
+    {
+        $filme->delete();
+
+        return redirect()
+            ->route('filmes.index')
+            ->with('success', 'Filme removido com sucesso.');
+    }
+}
